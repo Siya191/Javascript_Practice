1f8f11f3e8799d32f77c8cba326e14c7a5ded28f
Siya Patil. <115465247+Siya191@users.noreply.github.com>
2024-01-13 01:08:47 +0000

    container config done

---
+++ 01_Basics/04_stringtoNumber.js
@@ -0,0 +1,12 @@
+let str1 = "siya"
+let str2 = "patil"
+
+let str3  = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1+"2");
+console.log("1"+2 + 2);
+console.log(1+2+"4");
+
+console.log(+true);
